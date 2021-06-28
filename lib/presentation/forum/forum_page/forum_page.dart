66f4cf80497d37dce0655a9fbc7c03010734a0a3
Tan Le Yi <t.leyi2305@gmail.com>
2021-06-28 21:06:20 +0800

    Final for milestone 2 Co-authored-by: tanqiya <tanqiya@users.noreply.github.com>

--- lib/presentation/forum/forum_page/forum_page.dart
+++ lib/presentation/forum/forum_page/forum_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:friendlinus/application/forum/comment_watcher/comment_watcher_bloc.dart';
 import 'package:friendlinus/application/forum/forum_actor/forum_actor_bloc.dart';
 import 'package:friendlinus/application/forum/forum_post_watcher/forum_post_watcher_bloc.dart';
 import 'package:friendlinus/application/forum/forum_post_watcher/poll_watcher/poll_watcher_bloc.dart';
-import 'package:friendlinus/domain/data/forum/forum_post/forum_post.dart';
 import 'package:friendlinus/injection.dart';
-import 'package:friendlinus/presentation/core/app_bar.dart';
-import 'package:friendlinus/presentation/core/nav_bar.dart';
 import 'package:friendlinus/presentation/forum/forum_page/widgets/forum_body.dart';
 
 class ForumPage extends StatelessWidget {
@@ -24,6 +22,9 @@ class ForumPage extends StatelessWidget {
       BlocProvider(
           create: (context) =>
               getIt<ForumActorBloc>()..add(const ForumActorEvent.started())),
+      BlocProvider(
+          create: (context) => getIt<CommentWatcherBloc>()
+            ..add(CommentWatcherEvent.retrieveCommentsStarted(forumId))),
       if (pollAdded)
         BlocProvider(
             create: (context) => getIt<PollWatcherBloc>()
